5ccb65045a5dbee799f26cc8d3081151e9efa928
11Hamza <ST10268917@vcconnect.edu.za>
2025-10-06 00:39:32 +0200

    configuring for render

--- firebase.js
+++ firebase.js
@@ -1,24 +1,11 @@
 // firebase.js
-import 'dotenv/config';
 import admin from 'firebase-admin';
-import { readFileSync } from 'fs';
-
-let app;
 
 if (!admin.apps.length) {
-  // Load service account JSON from the path in the env var
-  const saPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-  if (!saPath) {
-    throw new Error('GOOGLE_APPLICATION_CREDENTIALS is not set');
-  }
-  const serviceAccount = JSON.parse(readFileSync(saPath, 'utf8'));
-
-  app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    projectId: process.env.FIREBASE_PROJECT_ID || serviceAccount.project_id,
+  // This uses the path in GOOGLE_APPLICATION_CREDENTIALS
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
   });
-} else {
-  app = admin.app();
 }
 
 export const db = admin.firestore();
